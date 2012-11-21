692c51a955acce018ca4c3d508e0eeb8c3878146
Misty De Meo <mistydemeo@gmail.com>
2012-11-21 16:51:06 -0600

    fidget: hide files away outside of lib/

--- fidget.rb
+++ fidget.rb
@@ -7,8 +7,11 @@ class Fidget < Formula
   sha1 '5f9d397307ca45fcf4fac9494d5380a47cd79f06'
 
   def install
+    inreplace "bin/fidget" do |s|
+      s.change_make_var! "BASEDIR", libexec
+    end
     bin.install "bin/fidget"
-    lib.install Dir['lib/*']
+    (libexec/"lib").install Dir['lib/*']
   end
 
   def test
