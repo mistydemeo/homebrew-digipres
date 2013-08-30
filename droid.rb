50314b891186ebf95c992e902e87b4efcd4803a0
Misty De Meo <mistydemeo@gmail.com>
2013-08-30 10:23:32 -0700

    droid 6.1.3

--- droid.rb
+++ droid.rb
@@ -2,20 +2,20 @@ require 'formula'
 
 class Droid < Formula
   homepage 'http://digital-preservation.github.com/droid/'
-  url 'http://www.nationalarchives.gov.uk/documents/information-management/droid-binary-6.1.2-bin.zip'
-  sha1 '3501e41b404caa30c74d298fd8ce9ad382141f81'
+  url 'http://www.nationalarchives.gov.uk/documents/information-management/droid-binary-6.1.3-bin.zip'
+  sha1 '88ddc08d7386e75291d9c5ecb0e43391e7c7b984'
 
   def install
     libexec.install Dir['*.jar']
     libexec.install "lib"
     inreplace "droid.sh" do |s|
-      s.gsub! "droid-command-line-6.1.2.jar", "#{libexec}/droid-command-line-6.1.2.jar"
-      s.gsub! "droid-ui-6.1.2.jar", "#{libexec}/droid-ui-6.1.2.jar"
+      s.gsub! "droid-command-line-6.1.3.jar", "#{libexec}/droid-command-line-6.1.3.jar"
+      s.gsub! "droid-ui-6.1.3.jar", "#{libexec}/droid-ui-6.1.3.jar"
     end
     bin.install "droid.sh" => "droid"
   end
 
   def test
-    system "#{bin}/droid", "-gt", "0"
+    system "#{bin}/droid", "--version"
   end
 end
