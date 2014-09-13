e67a09f6330060af46c78e965221ea937a2e430f
Andrew Jackson <Andrew.Jackson@bl.uk>
2014-09-13 20:33:48 +0100

    Fixed version.

--- siegfried.rb
+++ siegfried.rb
@@ -3,10 +3,11 @@ require 'formula'
 class Siegfried < Formula
   homepage 'http://www.itforarchivists.com/siegfried'
   url 'https://github.com/richardlehane/siegfried/releases/download/v0.4.1/siegfried_0-4-1_darwin64.zip'
-  sha1 '5f9d397307ca45fcf4fac9494d5380a47cd79f06'
+  version '0.4.1'
+  sha1 '8017bb067ab3a887afb193f52e43cbd1142c3c36'
 
   def install
-    bin.install "bin/sieg"
+    bin.install "sieg"
   end
 
   def test
