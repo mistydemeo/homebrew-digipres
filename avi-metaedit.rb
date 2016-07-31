087c5a5302d3e3a086ef240a7b5fe6556569c650
Misty De Meo <mistydemeo@gmail.com>
2016-07-31 12:14:46 +1000

    avi-metaedit: update autotools deps

--- avi-metaedit.rb
+++ avi-metaedit.rb
@@ -3,8 +3,8 @@ class AviMetaedit < Formula
   url "https://github.com/mistydemeo/AVI-MetaEdit/tarball/1.0.0"
   sha256 "cfc2d135a4b58108fdf0de9f8ba3b581e5c9f5a50fa891e87c745d3b8bd46af6"
 
-  depends_on :autoconf
-  depends_on :automake
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
 
   def install
     cd "Project/GNU/CLI" do
