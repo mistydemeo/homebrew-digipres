ec21253f78acdf81986d5dfdf0abb086f117b006
Misty De Meo <mistydemeo@gmail.com>
2016-07-31 11:05:25 +1000

    jwattools: modernize

--- jwattools.rb
+++ jwattools.rb
@@ -1,5 +1,3 @@
-require 'formula'
-
 #
 # Some inspiration taken from here:
 # https://github.com/mxcl/homebrew/blob/master/Library/Formula/jruby.rb
@@ -9,10 +7,10 @@ require 'formula'
 #
 
 class Jwattools < Formula
-  homepage 'https://sbforge.org/display/JWAT/JWAT-Tools'
-  url 'https://bitbucket.org/nclarkekb/jwat-tools/downloads/jwat-tools-0.5.6-SNAPSHOT.zip'
-  version '0.5.6-SNAPSHOT'
-  sha1 'bd639ba1d22ef6fc01f20dddafd19a8fbe5fd84b'
+  homepage "https://sbforge.org/display/JWAT/JWAT-Tools"
+  url "https://bitbucket.org/nclarkekb/jwat-tools/downloads/jwat-tools-0.5.6-SNAPSHOT.zip"
+  version "0.5.6-SNAPSHOT"
+  sha256 "36949ed3e80313b258d9ac4ced000177b70a0c35fb44343600c6c13fb52865d5"
 
   def install
     mv "jwattools.sh", "jwattools"
