06dbdd80c4228096225edf79d7ca94a7e455e899
Misty De Meo <mistydemeo@gmail.com>
2016-07-31 11:03:00 +1000

    fido: sha256

--- fido.rb
+++ fido.rb
@@ -1,17 +1,15 @@
-require 'formula'
-
 class Fido < Formula
-  homepage 'https://github.com/openplanets/fido'
-  url 'https://github.com/openplanets/fido/archive/1.3.1-78.tar.gz'
-  sha1 '0122a48d122c6d01af5a9f39b03f3bddc35250d3'
+  homepage "https://github.com/openplanets/fido"
+  url "https://github.com/openplanets/fido/archive/1.3.1-78.tar.gz"
+  sha256 "f8955faa1234c7e1f399892433ace1fd055cee883e1ab2a0d0ed98461c08c765"
   version "1.3.1-78"
 
-  head 'https://github.com/openplanets/fido.git'
+  head "https://github.com/openplanets/fido.git"
 
   def install
-    libexec.install Dir['fido/*']
+    libexec.install Dir["fido/*"]
     %w[fido.py prepare.py pronomutils.py toxml.py update_signatures.py].each do |f|
-      (bin/f.gsub('.py', '')).write <<-EOS.undent
+      (bin/f.gsub(".py", "")).write <<-EOS.undent
       #!/bin/sh
       exec python "#{libexec}/#{f}" "$@"
       EOS
