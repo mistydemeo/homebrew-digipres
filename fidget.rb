46de0337dde1818166a967247011e2dc73111f39
Misty De Meo <mistydemeo@gmail.com>
2016-07-31 10:56:05 +1000

    fidget: modernize

--- fidget.rb
+++ fidget.rb
@@ -1,17 +1,15 @@
-require 'formula'
-
 class Fidget < Formula
-  homepage 'https://github.com/openplanets/format-corpus'
-  url 'https://github.com/downloads/openplanets/format-corpus/fidget-0.0.3-SNAPSHOT-bin-unix.tar.gz'
-  version '0.0.3-snapshot'
-  sha1 '5f9d397307ca45fcf4fac9494d5380a47cd79f06'
+  homepage "https://github.com/openplanets/format-corpus"
+  url "https://github.com/downloads/openplanets/format-corpus/fidget-0.0.3-SNAPSHOT-bin-unix.tar.gz"
+  version "0.0.3-snapshot"
+  sha256 "f118a2a204e0f2ed9a9df139614c1dc9973724e8eddee910a12008cfa214051a"
 
   def install
     inreplace "bin/fidget" do |s|
       s.change_make_var! "BASEDIR", libexec
     end
     bin.install "bin/fidget"
-    (libexec/"lib").install Dir['lib/*']
+    (libexec/"lib").install Dir["lib/*"]
   end
 
   def test
