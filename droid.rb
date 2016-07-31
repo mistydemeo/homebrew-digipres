7162f9323ca90c73fffdf7398371da161c64261f
Misty De Meo <mistydemeo@gmail.com>
2016-07-31 10:54:46 +1000

    droid 6.2.1

--- droid.rb
+++ droid.rb
@@ -1,16 +1,15 @@
-require "formula"
-
 class Droid < Formula
+  desc "Java-based PRONOM file identification tool"
   homepage "http://digital-preservation.github.com/droid/"
-  url "http://www.nationalarchives.gov.uk/documents/information-management/droid-binary-6.1.5-bin.zip"
-  sha1 "ee9144ba720dfb5ac7578b6289e133fcd9f86d17"
+  url "http://nationalarchives.gov.uk/documents/information-management/droid-binary-6.2.1-bin.zip"
+  sha256 "45d8928f1425c1c0fbc0f8fb3dea9f07a0fe6848d0dd4696e15da1c52152d627"
 
   def install
     libexec.install Dir["*.jar"]
     libexec.install "lib"
     inreplace "droid.sh" do |s|
-      s.gsub! "droid-command-line-6.1.5.jar", "#{libexec}/droid-command-line-6.1.5.jar"
-      s.gsub! "droid-ui-6.1.5.jar", "#{libexec}/droid-ui-6.1.5.jar"
+      s.gsub! "droid-command-line-6.2.1.jar", "#{libexec}/droid-command-line-6.2.1.jar"
+      s.gsub! "droid-ui-6.2.1.jar", "#{libexec}/droid-ui-6.2.1.jar"
     end
     bin.install "droid.sh" => "droid"
   end
