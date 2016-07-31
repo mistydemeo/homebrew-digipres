21659ee0d1d37492f97d07d9521f001691500f29
Misty De Meo <mistydemeo@gmail.com>
2016-07-31 11:04:51 +1000

    jhove2: modernize

--- jhove2.rb
+++ jhove2.rb
@@ -1,15 +1,13 @@
-require 'formula'
-
 class Jhove2 < Formula
-  homepage 'https://bitbucket.org/jhove2/main/wiki/Home'
-  url 'http://bitbucket.org/jhove2/main/downloads/jhove2-2.1.0.tar.gz'
-  sha1 'f0159a536e4e3b6a56aa08a63cf9a0f5a3834ff0'
+  homepage "https://bitbucket.org/jhove2/main/wiki/Home"
+  url "http://bitbucket.org/jhove2/main/downloads/jhove2-2.1.0.tar.gz"
+  sha256 "d5036b76775b4c25d38c611f523010d65e426a70c809a04e7059b113ccd92087"
 
   def install
-    inreplace 'jhove2.sh' do |s|
-      s.change_make_var! 'ProgDir', libexec
+    inreplace "jhove2.sh" do |s|
+      s.change_make_var! "ProgDir", libexec
     end
-    libexec.install Dir['*']
-    (bin/'jhove2').make_relative_symlink libexec/'jhove2.sh'
+    libexec.install Dir["*"]
+    (bin/"jhove2").make_relative_symlink libexec/"jhove2.sh"
   end
 end
