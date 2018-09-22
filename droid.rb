97a74397f1ef9b030a4a050c0af423060cd59ed1
Misty De Meo <mistydemeo@gmail.com>
2018-09-21 19:02:56 -0700

    droid 6.4

--- droid.rb
+++ droid.rb
@@ -1,20 +1,20 @@
 class Droid < Formula
   desc "Java-based PRONOM file identification tool"
   homepage "http://digital-preservation.github.com/droid/"
-  url "http://nationalarchives.gov.uk/documents/information-management/droid-binary-6.2.1-bin.zip"
-  sha256 "45d8928f1425c1c0fbc0f8fb3dea9f07a0fe6848d0dd4696e15da1c52152d627"
+  url "http://www.nationalarchives.gov.uk/documents/information-management/droid-binary-6.4-bin.zip"
+  sha256 "9dd6289c1f03d8a9d628ab0127954e05acf4aa2f5f7d606b8a1b78996a98b815"
 
   def install
     libexec.install Dir["*.jar"]
     libexec.install "lib"
     inreplace "droid.sh" do |s|
-      s.gsub! "droid-command-line-6.2.1.jar", "#{libexec}/droid-command-line-6.2.1.jar"
-      s.gsub! "droid-ui-6.2.1.jar", "#{libexec}/droid-ui-6.2.1.jar"
+      s.gsub! "$DROID_HOME/droid-command-line-6.4.jar", "#{libexec}/droid-command-line-6.4.jar"
+      s.gsub! "$DROID_HOME/droid-ui-6.4.jar", "#{libexec}/droid-ui-6.4.jar"
     end
     bin.install "droid.sh" => "droid"
   end
 
   def test
-    system "#{bin}/droid", "--version"
+    assert_equal "6.4", shell_output("#{bin}/droid --version").chomp
   end
 end
