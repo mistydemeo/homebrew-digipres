72feb84ee801858a38876d51b58a22e1b7506cee
Misty De Meo <mistydemeo@gmail.com>
2022-06-19 16:04:47 -0700

    fluxengine 2022-06-17

--- fluxengine.rb
+++ fluxengine.rb
@@ -1,21 +1,23 @@
 class Fluxengine < Formula
   desc "PSOC5 floppy disk imaging interface"
   homepage "https://github.com/davidgiven/fluxengine"
-  url "https://github.com/davidgiven/fluxengine.git", revision: "d157b7b05d52ae85b7209c1d23d9a9f7f9b1df89"
-  version "2022-12-12"
+  url "https://github.com/davidgiven/fluxengine.git", revision: "3e934a8527b723fa9fff4a98db473f4bfce393df"
+  version "2022-06-17"
   license "MIT"
 
   depends_on "coreutils" => :build
+  depends_on "make" => :build
   depends_on "ninja" => :build
   depends_on "pkg-config" => :build
+  depends_on "fmt"
   depends_on "libusb"
   depends_on "protobuf"
   depends_on "sqlite3"
   depends_on "zlib"
 
   def install
-    system "make"
-    bin.install "brother120tool", "brother240tool", "fluxengine"
+    system "#{Formula["make"].opt_bin}/gmake", "fluxengine"
+    bin.install "fluxengine"
   end
 
   test do
