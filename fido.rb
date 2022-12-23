d0ebcce92963e86c36bd6b71cad7baf83d985db4
Misty De Meo <mistydemeo@gmail.com>
2022-12-22 16:43:07 -0800

    fido 1.6.1

--- fido.rb
+++ fido.rb
@@ -1,19 +1,56 @@
 class Fido < Formula
+  include Language::Python::Virtualenv
+
   homepage "https://github.com/openplanets/fido"
-  url "https://github.com/openplanets/fido/archive/1.3.1-78.tar.gz"
-  sha256 "f8955faa1234c7e1f399892433ace1fd055cee883e1ab2a0d0ed98461c08c765"
-  version "1.3.1-78"
+  url "https://files.pythonhosted.org/packages/26/34/7b997b3ba504d7ddb82007cda9cce37dc38db05ab58ba733a2eecd44cd60/opf-fido-1.6.1.tar.gz"
+  sha256 "b601650af4adca0b9d05274c39505f4c592cf868598aa09ad21857f70ceda795"
 
   head "https://github.com/openplanets/fido.git"
 
+  depends_on "python@3.11"
+
+  resource "certifi" do
+    url "https://files.pythonhosted.org/packages/37/f7/2b1b0ec44fdc30a3d31dfebe52226be9ddc40cd6c0f34ffc8923ba423b69/certifi-2022.12.7.tar.gz"
+    sha256 "35824b4c3a97115964b408844d64aa14db1cc518f6562e8d7261699d1350a9e3"
+  end
+
+  resource "charset-normalizer" do
+    url "https://files.pythonhosted.org/packages/a1/34/44964211e5410b051e4b8d2869c470ae8a68ae274953b1c7de6d98bbcf94/charset-normalizer-2.1.1.tar.gz"
+    sha256 "5a3d016c7c547f69d6f81fb0db9449ce888b418b5b9952cc5e6e66843e9dd845"
+  end
+
+  resource "idna" do
+    url "https://files.pythonhosted.org/packages/8b/e1/43beb3d38dba6cb420cefa297822eac205a277ab43e5ba5d5c46faf96438/idna-3.4.tar.gz"
+    sha256 "814f528e8dead7d329833b91c5faa87d60bf71824cd12a7530b5526063d02cb4"
+  end
+
+  resource "importlib-resources" do
+    url "https://files.pythonhosted.org/packages/1c/c8/cfc6ae38e378be60925f121cce01e7f4996dc3aca424799a693e48c9ce4d/importlib_resources-5.10.1.tar.gz"
+    sha256 "32bb095bda29741f6ef0e5278c42df98d135391bee5f932841efc0041f748dc3"
+  end
+
+  resource "olefile" do
+    url "https://files.pythonhosted.org/packages/34/81/e1ac43c6b45b4c5f8d9352396a14144bba52c8fec72a80f425f6a4d653ad/olefile-0.46.zip"
+    sha256 "133b031eaf8fd2c9399b78b8bc5b8fcbe4c31e85295749bb17a87cba8f3c3964"
+  end
+
+  resource "requests" do
+    url "https://files.pythonhosted.org/packages/a5/61/a867851fd5ab77277495a8709ddda0861b28163c4613b011bc00228cc724/requests-2.28.1.tar.gz"
+    sha256 "7c5599b102feddaa661c826c56ab4fee28bfd17f5abca1ebbe3e7f19d7c97983"
+  end
+
+  resource "six" do
+    url "https://files.pythonhosted.org/packages/71/39/171f1c67cd00715f190ba0b100d606d440a28c93c7714febeca8b79af85e/six-1.16.0.tar.gz"
+    sha256 "1e61c37477a1626458e36f7b1d82aa5c9b094fa4802892072e49de9c60c4c926"
+  end
+
+  resource "urllib3" do
+    url "https://files.pythonhosted.org/packages/c2/51/32da03cf19d17d46cce5c731967bf58de9bd71db3a379932f53b094deda4/urllib3-1.26.13.tar.gz"
+    sha256 "c083dd0dce68dbfbe1129d5271cb90f9447dea7d52097c6e0126120c521ddea8"
+  end
+
   def install
-    libexec.install Dir["fido/*"]
-    %w[fido.py prepare.py pronomutils.py toxml.py update_signatures.py].each do |f|
-      (bin/f.gsub(".py", "")).write <<-EOS.undent
-      #!/bin/sh
-      exec python "#{libexec}/#{f}" "$@"
-      EOS
-    end
+    virtualenv_install_with_resources
   end
 
   def test
