14699d24af919c1b897a786511499a1fc86cbea1
Misty De Meo <mistydemeo@gmail.com>
2023-12-12 14:41:15 -0800

    fluxengine 2023-12-12

--- fluxengine.rb
+++ fluxengine.rb
@@ -1,8 +1,8 @@
 class Fluxengine < Formula
   desc "PSOC5 floppy disk imaging interface"
   homepage "https://github.com/davidgiven/fluxengine"
-  url "https://github.com/davidgiven/fluxengine.git", revision: "90da6b1e726db0cc960613b34a1d192b9e0e5817"
-  version "2023-08-08"
+  url "https://github.com/davidgiven/fluxengine.git", revision: "7462bd995f3a8bff2c75549b8dc2220b5539376d"
+  version "2023-12-12"
   license "MIT"
   head "https://github.com/davidgiven/fluxengine.git"
 
@@ -10,8 +10,8 @@ class Fluxengine < Formula
   depends_on "make" => :build
   depends_on "ninja" => :build
   depends_on "pkg-config" => :build
+  depends_on "abseil"
   depends_on "fmt"
-  depends_on "libusb"
   depends_on "protobuf"
   depends_on "sqlite3"
   depends_on "zlib"
