1f4cbb76f9a55e7912ebca8085b0671474383853
Misty De Meo <mistydemeo@gmail.com>
2024-10-21 21:39:51 -0700

    disc-image-creator 2024-10-01

--- disc-image-creator.rb
+++ disc-image-creator.rb
@@ -1,8 +1,8 @@
 class DiscImageCreator < Formula
   desc "Low-level optical disc archiving tool"
   homepage "https://github.com/saramibreak/DiscImageCreator"
-  url "https://github.com/saramibreak/DiscImageCreator/files/13801554/DiscImageCreator_20240101.tar.gz"
-  sha256 "ea06cfd06dcf968746a38598812867ae6fd9356fb0f58612717c8df4d3336781"
+  url "https://github.com/user-attachments/files/17211442/DiscImageCreator_20241001.zip"
+  sha256 "8bec694331f9faa546880631d3f675d92d56b5f27d4bfcb1252656b0c31db953"
   license "Apache-2.0"
 
   def install
