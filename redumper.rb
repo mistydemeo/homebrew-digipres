6378294be555b135eafd9b963242a74c37a3cbbe
Misty De Meo <mistydemeo@gmail.com>
2024-12-05 20:40:25 -0800

    redumper r438

--- redumper.rb
+++ redumper.rb
@@ -2,13 +2,13 @@ class Redumper < Formula
   desc "Low level CD dumper utility"
   homepage "https://github.com/superg/redumper"
   # From-source build currently requires a newer clang than Apple ships
-  url "https://github.com/superg/redumper/releases/download/build_428/redumper-2024.10.20_build428-Darwin.zip"
-  version "428"
-  sha256 "49edb7023b1b11c0290fd9bc5c7cdbb68d77a41d4f123883638a84e4c016b2e3"
+  url "https://github.com/superg/redumper/releases/download/build_438/redumper-2024.11.03_build438-Darwin.zip"
+  version "438"
+  sha256 "a046f7bcc79bde646a13b25888e6c794d8b7ec7aa585613ebe7093fc7433de48"
   license "GPL-3.0-only"
 
   # Links against a Homebrew-provided libc++
-  depends_on "llvm"
+  depends_on "llvm@18"
 
   def install
     bin.install Dir["bin/*"]
