25672d309c20c6afe3ecf954e05208d0fd7e8c3b
Jonathan Dahan <github@jonathan.is>
2025-01-29 15:48:37 -0500

    fluxengine 2024-11-24
    Co-authored-by: Jonathan Dahan <git@jonathan.is>

--- fluxengine.rb
+++ fluxengine.rb
@@ -1,8 +1,8 @@
 class Fluxengine < Formula
   desc "PSOC5 floppy disk imaging interface"
   homepage "https://github.com/davidgiven/fluxengine"
-  url "https://github.com/davidgiven/fluxengine.git", revision: "7462bd995f3a8bff2c75549b8dc2220b5539376d"
-  version "2023-12-12"
+  url "https://github.com/davidgiven/fluxengine.git", revision: "9ce405cec50ded5f64774d2448d9438a7e20b713"
+  version "2024-11-24"
   license "MIT"
   head "https://github.com/davidgiven/fluxengine.git"
 
