2afd92a7ca09e05671476c4b7c32a8105e132086
Misty De Meo <mistydemeo@gmail.com>
2025-03-31 20:25:22 -0700

    redumper r537

--- redumper.rb
+++ redumper.rb
@@ -2,9 +2,9 @@ class Redumper < Formula
   desc "Low level CD dumper utility"
   homepage "https://github.com/superg/redumper"
   # From-source build currently requires a newer clang than Apple ships
-  url "https://github.com/superg/redumper/releases/download/build_533/redumper-2025.03.27_build533-Darwin64.zip"
-  version "533"
-  sha256 "b152b7a5885f2a73aebe36e3e59e43c336c9ef416c26d4d5cfe39f607a25b679"
+  url "https://github.com/superg/redumper/releases/download/build_537/redumper-2025.03.29_build537-Darwin64.zip"
+  version "537"
+  sha256 "af054396a8511ade80ec3c8b0904244bbbb4fafec39e00a3419a583f647f1aec"
   license "GPL-3.0-only"
 
   # Links against a Homebrew-provided libc++
