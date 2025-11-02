777368bbf164fa52ada1c9d1e5ecf2bf84d9a69c
Misty De Meo <mistydemeo@gmail.com>
2025-11-01 19:53:55 -0700

    redumper r658

--- redumper.rb
+++ redumper.rb
@@ -2,16 +2,15 @@ class Redumper < Formula
   desc "Low level CD dumper utility"
   homepage "https://github.com/superg/redumper"
   # From-source build currently requires a newer clang than Apple ships
-  url "https://github.com/superg/redumper/releases/download/build_537/redumper-2025.03.29_build537-Darwin64.zip"
-  version "537"
-  sha256 "af054396a8511ade80ec3c8b0904244bbbb4fafec39e00a3419a583f647f1aec"
+  url "https://github.com/superg/redumper/releases/download/b658/redumper-b658-macos-arm64.zip"
+  version "658"
+  sha256 "d8d29762d8f0944d96984b9125894514dd71ec646265d5feeff5609f9580e829"
   license "GPL-3.0-only"
 
-  # Links against a Homebrew-provided libc++
-  depends_on "llvm@18"
-
   def install
-    bin.install Dir["bin/*"]
+    # Bundles a libc++ in lib, which we want to avoid making public
+    libexec.install Dir["*"]
+    bin.install_symlink libexec/"bin/redumper"
   end
 
   test do
